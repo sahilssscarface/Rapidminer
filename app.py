83887aba5901cab78c608b26562064d344592589
sahilssscarface <68786756+sahilssscarface@users.noreply.github.com>
2023-09-20 16:17:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 # app.py
-
+!pip install pypmml
+!pip install streamlit
 import streamlit as st
-import pyPMML
-from pyPMML import Model
+from pypmml import Model
+
+
+# Ensure you have the correct path to your PMML file here
+pmml_file_path = 'test3.0.pmml'
 
 # Load PMML Model
-model = Model.load("test3.0.pmml")
+model = Model.fromFile(pmml_file_path)
 
 # Create a function to predict selling price
 def predict_price(car_model, age, mileage):
@@ -13,18 +17,18 @@ def predict_price(car_model, age, mileage):
     bmw_x5 = 1 if car_model == "BMW X5" else 0
     audi_a5 = 1 if car_model == "Audi A5" else 0
     mercedes_benz_c = 1 if car_model == "Mercedes Benz C class" else 0
-
     # Construct input data based on the format expected by your model
     input_data = {
         "BMW X5": bmw_x5,
         "Audi A5": audi_a5,
-        "Age(yrs)": age,
+        "Age": age,
         "Mileage": mileage
     }
-    
+
     # Use the model to predict
-    result = model.predict(input_data)
-    return result[0]  # assuming it returns a list, get the first item which is the predicted price.
+    result = model.predicy(input_data)   
+    return result[0]
+      # assuming it returns a list, get the first item which is the predicted price.
 
 # Streamlit App UI
 st.title("Car Selling Price Predictor")
@@ -40,4 +44,3 @@ mileage = st.number_input("Enter Mileage of the Car", min_value=0, max_value=100
 if st.button("Predict Selling Price"):
     price = predict_price(car_model, age, mileage)
     st.write(f"The estimated selling price is: ${price}")
-
